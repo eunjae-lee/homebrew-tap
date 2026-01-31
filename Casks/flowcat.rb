6a71c6727462f4bfce3c06e158b334ecf8d3780e
Eunjae Lee <hey@eunjae.dev>
2026-01-31 22:41:43 +0100

    Fix flowcat SHA256

--- Casks/flowcat.rb
+++ Casks/flowcat.rb
@@ -1,6 +1,6 @@
 cask "flowcat" do
   version "1.4.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "360b72116effb4dc0e46d0d8304ce55550482a8199b18340112694089e7b5c46"
 
   url "https://github.com/eunjae-lee/flowcat/releases/download/v#{version}/Flowcat.dmg"
   name "Flowcat"
